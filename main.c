722f4dee5ec161f03ce334b7aee56f341050b92d
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-02 11:38:29 +0200

    Implementation fonction compteur de lignes dans le fichier

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <ctype.h>
+#include "main.h"
 
 int			main(void)
 {
+	
 	return (0);
+}
+
+char		readChar()
+{
+	char 	toRead;
+
+	toRead = 0;
+	toRead = getchar();
+	toRead = toupper(toRead);
+
+	while (getchar() != '\n');
+
+	return (toRead);
+}
+
+int			lenFile(char *filename)
+{
+	FILE 	*file;
+	char 	toRead;
+	int		len;
+
+	file = NULL;
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (-1);
+	toRead = 0;
+	len = 0;
+	while ((toRead = fgetc(file)) != EOF)
+		if (toRead == '\n')
+			len++;
+	fclose(file);
+	return (len + 1);
 }
---
+++ main.h
@@ -0,0 +1,2 @@
+char		readChar();
+int			lenFile(char *filename);
