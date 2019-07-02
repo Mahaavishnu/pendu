ee8660f1352e62bc91f8c01dd50617d73684aa03
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-02 13:20:56 +0200

    Programme fonctionnel sans option

--- main.c
+++ main.c
@@ -2,20 +2,46 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <time.h>
-#include "string.h"
+#include <string.h>
 #include "main.h"
 
 int			main(void)
 {
 	int 	wordNumber;
 	char 	*mysteryWord;
+	int		tries;
+	int		*tab;
+	int		len;
 
 	srand(time(NULL));
 	mysteryWord = NULL;
+	tries = 10;
 	wordNumber = rand() % lenFile("dico.txt") + 1;
 	printf("Mot numéro : %d \n", wordNumber);
 	mysteryWord = extractWord(wordNumber, "dico.txt");
-	printf("Mot extrait : %s\n", mysteryWord);
+	len = strlen(mysteryWord);
+	tab = (int *)malloc(sizeof(int)*len);
+	while (len)
+	{
+		tab[len-1] = 0;
+		len--;
+	}
+	//printf("Mot extrait : %s\n", mysteryWord);
+	printf("Bienvenu dans le Pendu !\n\n");
+	while (tries)
+	{
+		printf("Il vous reste %d coups à jouer.\nQuel est le mot secret ? ", tries);
+		printWord(mysteryWord, tab);
+		printf("Proposez une lettre : ");
+		if (guessChar(mysteryWord, tab) == 0)
+			tries--;
+		else if (testEndGame(mysteryWord, tab) == 1)
+		{
+			printf("Gagné ! le mot secret était bien : %s.\n", mysteryWord);
+			return (0);
+		}
+		printf("\n");
+	}
 	free(mysteryWord);
 	return (0);
 }
@@ -75,7 +101,61 @@ char		*extractWord(int wordNumber, char *filename)
 	printf("Longueur du mot : %d\n", len);
 	if (fgets(mysteryWord, len+1, file) == NULL)
 		printf("problème avec fgets");
-	printf("Mot extrait : %s\n", mysteryWord);
+	//printf("Mot extrait : %s\n", mysteryWord);
 	fclose(file);
 	return (mysteryWord);
+}
+
+void		printWord(char *mysteryWord, int *tab)
+{
+	int		len;
+	int		pos;
+
+	len = strlen(mysteryWord);
+	pos = 0;
+	while (pos != len)
+	{
+		if (tab[pos] == 1)
+			printf("%c", mysteryWord[pos]);
+		else
+			printf("*");
+		pos++;
+	}
+	printf("\n");
+}
+
+int			guessChar(char *mysteryWord, int *tab)
+{
+	int		len;
+	char 	guess;
+	int 	result;
+
+	len = strlen(mysteryWord);
+	guess = readChar();
+	result = 0;
+	while (len)
+	{
+		if (mysteryWord[len-1] == guess)
+		{
+			tab[len-1] = 1;
+			result = 1;
+		}
+		len--;
+	}
+	return (result);
+}
+
+int		testEndGame(char *mysteryWord, int *tab)
+{
+	int	len;
+
+	len = 0;
+	len = strlen(mysteryWord);
+	while (len)
+	{
+		if (tab[len-1] == 0)
+			return (0);
+		len--;
+	}
+	return (1);
 }
--- main.h
+++ main.h
@@ -1,3 +1,6 @@
 char		readChar();
 int			lenFile(char *filename);
 char		*extractWord(int wordNumber, char *filename);
+void		printWord(char *mysteryWord, int *tab);
+int			guessChar(char *mysteryWord, int *tab);
+int			testEndGame(char *mysteryWord, int *tab);
